9f2e03d539bdfd05b79a7cb7feec7c87b4297505
Kurk3 <kurekadam314@gmail.com>
2022-04-16 19:36:22 +0200

    uprava a fixovanie bugov

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -162,14 +162,14 @@ $(document).ready(function () {
     });
 });
 
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+// function myFunction() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//         x.className += "responsive";
+//     } else {
+//         x.className = "topnav";
+//     }
+// }
 
 //
 
